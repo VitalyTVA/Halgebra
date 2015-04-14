6c026ef1e675af0dee61d18849360498e58bfe34
Vitaly Tolstikov <vitalytva@yanex.ru>
2015-04-14 09:16:31 -0700

    t

--- src/Halgebra/ExprTree.hs
+++ src/Halgebra/ExprTree.hs
@@ -5,5 +5,9 @@ import Data.Tree
 leaf :: a -> Tree a
 leaf a = Node a []
 
+node :: a -> [a] -> Tree a
+node a ts = Node a $ fmap leaf ts
+
 parseTree :: String -> Tree String
-parseTree a = leaf a 
+parseTree a = node (head ws) $ (tail ws)
+                where ws = words a
