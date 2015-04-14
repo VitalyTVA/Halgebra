a4b850459cb14bcb27846f5199e2d2dc573cc6e8
Vitaly Tolstikov <vitalytva@yanex.ru>
2015-04-14 11:26:07 -0700

    t

--- src/Halgebra/ExprTree.hs
+++ src/Halgebra/ExprTree.hs
@@ -1,7 +1,7 @@
 module Halgebra.ExprTree (parseTree, leaf) where
 
 import Data.Tree
-import Debug.Trace
+--import Debug.Trace
 
 leaf :: a -> Tree a
 leaf a = Node a []
@@ -13,15 +13,15 @@ parseTree :: String -> Tree String
 parseTree a = snd (parseTreeCore $ words a)
                 
 parseTreeCore :: [String] -> ([String], Tree String)
-parseTreeCore (x:xs) = ((fst forest), Node x (snd forest))
+parseTreeCore (x:xs) = (fst forest, Node x (snd forest))
                         where forest = parseForest (xs, []) 
 parseTreeCore _ = error "No leaf"
 
 parseForest :: ([String], Forest String) -> ([String], Forest String)
 parseForest ([], forest) = ([], forest)
 parseForest ([")"], forest) = ([], forest)
-parseForest ([x], forest) = ([], (forest ++ [(leaf x)]))
-parseForest ((")":xs), forest) = (xs, forest)
-parseForest (("(":xs), forest) = parseForest (fst tree, (forest ++ [snd tree]))
+parseForest ([x], forest) = ([], forest ++ [leaf x])
+parseForest (")" : xs, forest) = (xs, forest)
+parseForest ("(" : xs, forest) = parseForest (fst tree, forest ++ [snd tree])
                         where tree = parseTreeCore xs
-parseForest ((x:xs), forest) = parseForest (xs, (forest ++ [(leaf x)]))
+parseForest (x : xs, forest) = parseForest (xs, forest ++ [leaf x])
