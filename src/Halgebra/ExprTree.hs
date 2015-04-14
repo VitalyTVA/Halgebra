55b17e4fc0c77f18ec0f7c2044617a5a1df5a43f
Vitaly Tolstikov <vitalytva@yanex.ru>
2015-04-14 12:15:38 -0700

    t

--- src/Halgebra/ExprTree.hs
+++ src/Halgebra/ExprTree.hs
@@ -1,20 +1,41 @@
-module Halgebra.ExprTree (parseTree, leaf) where
+module Halgebra.ExprTree (parseTree, evalExpression, leaf) where
 
 import Data.Tree
 --import Debug.Trace
 
-leaf :: a -> Tree a
-leaf a = Node a []
 
---node :: a -> [a] -> Tree a
---node a ts = Node a $ fmap leaf ts
+type EvalResult = Integer
+
+data Term = Term { 
+        name :: String,
+        eval :: [Term] -> EvalResult
+        }
+        
+instance Show Term where
+    show = name
+
+evalExpression :: String -> EvalResult
+evalExpression = evalTree.parseTerms
+
+evalTree :: Tree Term -> EvalResult
+evalTree (Node t _) = (eval t) []--(fmap rootLabel ts)   
+
+parseTerm :: String -> Term
+--parseTerm "+" = Term "+" (\_ -> 1)
+parseTerm a = Term a (\_ -> read a)
+
+parseTerms :: String -> Tree Term
+parseTerms a = fmap parseTerm $ parseTree a
+
+leaf :: a -> Tree a
+leaf a = Node a [] 
 
 parseTree :: String -> Tree String
 parseTree a = snd (parseTreeCore $ words a)
                 
 parseTreeCore :: [String] -> ([String], Tree String)
-parseTreeCore (x:xs) = (fst forest, Node x (snd forest))
-                        where forest = parseForest (xs, []) 
+parseTreeCore (x:xs) = (left, Node x forest)
+                        where (left, forest) = parseForest (xs, []) 
 parseTreeCore _ = error "No leaf"
 
 parseForest :: ([String], Forest String) -> ([String], Forest String)
@@ -22,6 +43,6 @@ parseForest ([], forest) = ([], forest)
 parseForest ([")"], forest) = ([], forest)
 parseForest ([x], forest) = ([], forest ++ [leaf x])
 parseForest (")" : xs, forest) = (xs, forest)
-parseForest ("(" : xs, forest) = parseForest (fst tree, forest ++ [snd tree])
-                        where tree = parseTreeCore xs
+parseForest ("(" : xs, forest) = parseForest (left, forest ++ [subTree])
+                        where (left, subTree) = parseTreeCore xs
 parseForest (x : xs, forest) = parseForest (xs, forest ++ [leaf x])
--- test/Halgebra/ExprTreeTests.hs
+++ test/Halgebra/ExprTreeTests.hs
@@ -13,3 +13,7 @@ test_parseTree = do
                 assertEqual (Node "xx" [Node "aa" [leaf "bb"], leaf "cc"]) (parseTree "xx ( aa bb ) cc")
                 assertEqual (Node "xx" [Node "aa" [leaf "bb"]]) (parseTree "xx ( aa bb )")
                 assertEqual (Node "xx" [leaf "ee", Node "aa" [leaf "dd", Node "bb" [leaf "ff"]], leaf "cc"]) (parseTree "xx ee ( aa dd ( bb ff ) ) cc")
+                
+test_evalTree = do
+                assertEqual 1 $ evalExpression "1"
+                assertEqual 100 $ evalExpression "100"
